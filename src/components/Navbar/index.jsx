6112bcaa8600775695eebef4f62010cd3b364616
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-14 14:48:41 +0300

    Add initialiser code for navbar component

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className="navbar-container">
+            <header>left header</header>
+            <header>right header</header>
+        </div>
+    )
+}
+
+export default Navbar
