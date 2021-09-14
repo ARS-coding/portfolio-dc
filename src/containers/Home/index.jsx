402f15332098e8104508ab8221efbe99686ac3a6
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-14 15:07:18 +0300

    Add Home folder to the containers folder

---
+++ src/containers/Home/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Container } from "react-bootstrap";
+
+import Navbar from "../../components/Navbar";
+
+function Home() {
+    return (
+        <Container fluid>
+            <Navbar />            
+        </Container>
+    )
+}
+
+export default Home
