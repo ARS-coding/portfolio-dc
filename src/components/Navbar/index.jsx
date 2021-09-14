837e326157debf759c7cca6f8e538319a5819257
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-14 15:07:40 +0300

    Import navbar into home, import home intp app

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
+import { Container, Row, Col } from "react-bootstrap";
+
 function Navbar() {
     return (
-        <div className="navbar-container">
-            <header>left header</header>
-            <header>right header</header>
-        </div>
+        <Container fluid className="navbar-container">
+            <Row>
+                <Col xs={12} sm={12} md={12} lg={6}>left header</Col>
+                <Col xs={12} sm={12} md={12} lg={6}>right header</Col>
+            </Row>
+        </Container>
     )
 }
 
