4b77da28fa4dcdebb97a864877466f6e8f605fb6
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-15 20:51:38 +0300

    Finalise hero section layout

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -4,24 +4,24 @@ import { Container } from "react-bootstrap";
 
 import PointDownIMG from "../../images/point-down.png";
 import { ReactComponent as HeroShapesSVG } from "../../images/hero-shapes.svg";
+import { ReactComponent as ScrollIconSVG } from "../../images/scroll-icon.svg";
 
 import "./index.scss";
 
 function Hero() {
     return (
-        <Container fluid className="hero px-3 px-sm-0">
-            <Container className="hero-content">
-                <div className="content">
-                    <h1>Beautifully Created Web Experiences</h1>
-                    <div className="meet">
+        <Container fluid className="hero-container px-3 px-sm-0 d-flex">
+            <Container className="hero-content-container ms-lg-0 d-flex justify-content-center align-items-center">
+                <div className="text-center">
+                    <h1 className="mx-auto"><span>Beautifully</span> Crafted<br/>Web Experiences</h1>
+                    <div className="meet mx-auto">
                         <img src={PointDownIMG} alt="A hand pointing down" />
-                        <p>Meet Ali Rıza Şahin</p>
+                        <p><span>Meet</span> Ali Rıza Şahin</p>
                     </div>
-
-                    {/* add scrolling icon svg */}
+                    <ScrollIconSVG className="mx-auto" />
                 </div>
                 
-                <HeroShapesSVG />
+                <HeroShapesSVG className="d-none d-sm-none d-md-none d-lg-inline" />
             </Container>
         </Container>
     )
