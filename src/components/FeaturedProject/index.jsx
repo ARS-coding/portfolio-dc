3fb14459a66264853c7ef3788a09f6284e285357
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-16 17:56:08 +0300

    Add starter code for featured project section

---
+++ src/components/FeaturedProject/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import { Container, Row, Col } from "react-bootstrap";
+
+import LTNMockupImage from "../../images/LTN-mockup-image.jpg"
+
+import "./index.scss";
+
+function FeaturedProject() {
+    return (
+        <Container fluid className="featured-project-section">
+            <Container className="featured-project-content-container">
+                <Row className="featured-project-content-row d-flex justify-content-center align-items-center">
+                    <Col>
+                        <span>Featured Project</span>
+                        <br/>
+                        <a href="https://lovethyneighbor.netlify.app/" target="_blank" rel="noreferrer">Love Thy Neighbor</a>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla, necessitatibus, fuga laudantium tempore veniam recusandae debitis qui error iusto dolor nam animi ea voluptas sed nesciunt labore incidunt. Ea, reiciendis.</p>
+                    </Col>
+                    <Col>
+                        <img src={LTNMockupImage} alt="Love Thy Neighbor mockup" />
+                    </Col>
+                </Row>
+            </Container>
+        </Container>
+    )
+}
+
+export default FeaturedProject
