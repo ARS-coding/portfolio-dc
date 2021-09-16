ec2f62460eb1ebf437b105b91b5bfd26810a60bb
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-16 20:42:14 +0300

    Add the animations for the featured project section and finish the desktop version

--- src/components/FeaturedProject/index.jsx
+++ src/components/FeaturedProject/index.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import { Container, Row, Col } from "react-bootstrap";
 
@@ -7,18 +10,43 @@ import LTNMockupImage from "../../images/LTN-mockup-image.jpg"
 import "./index.scss";
 
 function FeaturedProject() {
+    
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+        gsap.from(".column-left",
+            {  
+                x: -100,
+                duration: 2,
+                scrollTrigger: {
+                    trigger: ".featured-project-content-row", toggleActions: "play none none pause", start: "29% bottom" 
+                } 
+            }
+        );
+        gsap.from(".column-right",
+            { 
+                scrollTrigger: { 
+                    trigger: ".featured-project-content-row", toggleActions: "play none none pause", start: "29% bottom" 
+                }, 
+                x: 100,
+                duration: 2,
+            }
+        );
+    }, []);
+
     return (
         <Container fluid className="featured-project-section">
             <Container className="featured-project-content-container">
                 <Row className="featured-project-content-row d-flex justify-content-center align-items-center">
-                    <Col>
+                    <Col className="column-left">
                         <span>Featured Project</span>
                         <br/>
                         <a href="https://lovethyneighbor.netlify.app/" target="_blank" rel="noreferrer">Love Thy Neighbor</a>
                         <p><span>Love Thy Neighbor</span> helps people to find neighbors nearby and invite them to an event so they could build a dynamic community that uplifts each other.</p>
                     </Col>
-                    <Col>
-                        <img src={LTNMockupImage} alt="Love Thy Neighbor mockup" />
+                    <Col className="column-right">
+                        <a href="https://lovethyneighbor.netlify.app/" target="_blank" rel="noreferrer">
+                            <img src={LTNMockupImage} alt="Love Thy Neighbor mockup" />
+                        </a>
                     </Col>
                 </Row>
             </Container>
