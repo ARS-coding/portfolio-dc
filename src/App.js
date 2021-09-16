527fcb677288472cebf2fc798c314f39bc5a2874
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-16 20:02:48 +0300

    Register scroll trigger plugin of gsap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import React, { useEffect } from "react";
+
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 import Home from "./containers/Home";
 
 import "./main.scss";
 
 function App() {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+  }, []) 
+
   return (
     <div className="App">
       <Home />
