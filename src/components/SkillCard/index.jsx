61b356c4a57bace858b11417652e4124bc1c6f11
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-17 22:04:06 +0300

    Finalise skill cards

--- src/components/SkillCard/index.jsx
+++ src/components/SkillCard/index.jsx
@@ -7,7 +7,7 @@ import "./index.scss";
 function SkillCard({ Image, title, text }) {
     return (
         <Col xs={12} sm={12} md={6} lg={3} className="skill-card-column">
-            <Card className="d-flex flex-column justify-content-center mx-2 mb-3">
+            <Card className="d-flex flex-column justify-content-center mx-2 mb-3 mb-sm-3 mb-md-3 mb-lg-0">
                 {<Image />}
                 <div className="pt-2 px-3">
                     <Card.Title>{title}</Card.Title>
