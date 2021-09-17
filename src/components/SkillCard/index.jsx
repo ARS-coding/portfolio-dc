a56bbc3902faccbfecafd54f9cc15ebdd7ac7583
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-17 23:01:59 +0300

    Show different animations on different screen sizes

--- src/components/SkillCard/index.jsx
+++ src/components/SkillCard/index.jsx
@@ -4,9 +4,9 @@ import { Col, Card } from "react-bootstrap";
 
 import "./index.scss";
 
-function SkillCard({ Image, title, text }) {
+function SkillCard({ Image, title, text, className }) {
     return (
-        <Col xs={12} sm={12} md={6} lg={3} className="skill-card-column">
+        <Col xs={12} sm={12} md={6} lg={3} className={`skill-card-column ${className}`}>
             <Card className="d-flex flex-column justify-content-center mx-2 mb-3 mb-sm-3 mb-md-3 mb-lg-0">
                 {<Image />}
                 <div className="pt-2 px-3">
--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import { Container, Row } from "react-bootstrap";
 
@@ -12,6 +15,47 @@ import SkillCard from "../SkillCard";
 import "./index.scss";
 
 function Skills() {
+
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+
+        if (window.innerWidth >= 992) {
+            gsap.from(".skill-card-column",
+                { 
+                    duration: 1.5,
+                    y: 50,
+                    opacity: 0,
+                    stagger: .4,
+                    scrollTrigger: {
+                        trigger: ".skill-card-column",
+                        toggleActions: "play none none pause",
+                        start: "35% bottom"
+                    }
+                }
+            );
+        }
+        else {
+            function animateSkillCardColumns() {
+                const skillCardColumns = document.querySelectorAll(".skill-card-column");
+                
+                skillCardColumns.forEach((skillCardColumn, index) => {
+                    gsap.from(`.skill-card-column-${index}`,
+                        { 
+                            duration: 2.7,
+                            x: (index % 2) === 0 ? -100 : 100,
+                            scrollTrigger: {
+                                trigger: `.skill-card-column-${index}`,
+                                toggleActions: "play none none pause",
+                                start: "40% bottom"
+                            }
+                        }
+                    )
+                })
+            }
+            animateSkillCardColumns()
+        }
+    }, [])
+
     return (
         <section className="skills-section container-fluid">
             <Container className="skills-content-container px-0">
@@ -20,20 +64,24 @@ function Skills() {
                         Image={ResponsiveIconSVG} 
                         title="Responsiveness" 
                         text="Fluency in creating accessible and responsive websites with the help of most up to date technologies."
+                        className="skill-card-column-0"
                     />
                     <SkillCard Image={JSIconSVG} 
                         title="JavaScript"
                         text="A good grip of ES6, using external RESTful APIs, OOP, and JavaScript fundamentals by building projects."
+                        className="skill-card-column-1"
                     />
                     <SkillCard 
                         Image={ReactIcon}
                         title="React"
                         text="Hands-on practiced knowledge in separation of concerns, component oriented thinking, hooks and JSX."
+                        className="skill-card-column-2"
                     />
                     <SkillCard 
                         Image={ReduxIcon}
                         title="Redux"
                         text="Confident in managing and enhancing the data flow of an app by creating meaningful data structures."
+                        className="skill-card-column-3"
                     />
                 </Row>
             </Container>
