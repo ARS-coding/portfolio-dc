07573512c72605723f6d323e044db7bf5352c1dd
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-17 20:51:33 +0300

    Use a normal div instead of Card.Body component

--- src/components/SkillCard/index.jsx
+++ src/components/SkillCard/index.jsx
@@ -7,12 +7,12 @@ import "./index.scss";
 function SkillCard({ Image, title, text }) {
     return (
         <Col xs={12} sm={12} md={6} lg={3} className="skill-card-column">
-            <Card>
+            <Card className="d-flex flex-column justify-content-center mx-2 mb-3">
                 {<Image />}
-                <Card.Body>
+                <div className="pt-2 px-3">
                     <Card.Title>{title}</Card.Title>
                     <Card.Text>{text}</Card.Text>
-                </Card.Body>
+                </div>
             </Card>
         </Col>
     )
