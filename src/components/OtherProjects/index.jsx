4eea7120dc0831b8e5d61e97e76035b41a3350c7
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-17 23:49:49 +0300

    Add starter code for the layout of other projects section

---
+++ src/components/OtherProjects/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { Container } from "react-bootstrap";
+
+import "./index.scss";
+
+function OtherProjects() {
+    return (
+        <section className="other-projects-section container-fluid">
+            <Container className="other-projects-content-container px-0">
+                <p>hey</p>
+            </Container>
+        </section>
+    )
+}
+
+export default OtherProjects
