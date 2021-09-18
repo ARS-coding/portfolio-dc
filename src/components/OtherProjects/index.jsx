f3b8f8cf4008146c9f63a9d32f2f6c4db21a8d1e
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-18 23:15:31 +0300

    Remove unwanted horizontal paddings

--- src/components/OtherProjects/index.jsx
+++ src/components/OtherProjects/index.jsx
@@ -8,7 +8,7 @@ import "./index.scss";
 
 function OtherProjects() {
     return (
-        <section className="other-projects-section container-fluid">
+        <section className="other-projects-section container-fluid px-0">
             <Container className="other-projects-content-container px-0">
                 <OtherProject />
             </Container>
