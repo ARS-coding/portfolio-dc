a514191b80a2e50608ba5b5d5225c142150307f9
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-18 14:44:15 +0300

    Add other project component into other projects component

--- src/components/OtherProjects/index.jsx
+++ src/components/OtherProjects/index.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 import { Container } from "react-bootstrap";
 
+import OtherProject from "../OtherProject";
+
 import "./index.scss";
 
 function OtherProjects() {
     return (
         <section className="other-projects-section container-fluid">
             <Container className="other-projects-content-container px-0">
-                <p>hey</p>
+                <OtherProject />
             </Container>
         </section>
     )
