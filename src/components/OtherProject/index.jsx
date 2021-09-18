cac2502d628fad18f20e6700a4931121375ed35e
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-18 23:15:12 +0300

    Add basic content and styling

--- src/components/OtherProject/index.jsx
+++ src/components/OtherProject/index.jsx
@@ -10,10 +10,11 @@ function OtherProject() {
             <Container className="other-project-content-container px-0">
                 <Row className="other-project-content-row">
                     <Col>
-                    
+                        <span>Web App</span>
+                        <a href="https://oop-tmdb-movie-website.web.app/" target="_blank" rel="noreferrer">OOP The Movie Data Base Website</a>
+                        <p>With the help of the TMDB API, <a href="https://www.linkedin.com/in/ufuk-deniz-demirbilek-226636135/" target="_blank" rel="noreferrer">Ufuk Deniz Demirbilek</a> and me created this website with OOP principles. Website presents detailed data about large amount of movies in a user friendly manner and enables users to learn more about the spesific movie.</p>
                     </Col>
                     <Col>
-                    
                     </Col>
                 </Row>
             </Container>
