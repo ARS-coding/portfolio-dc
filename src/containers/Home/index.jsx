857dfbf0399cf163575ca11627e9bc08f873f32a
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-19 18:33:40 +0300

    Import github-calendar into home

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -7,6 +7,7 @@ import Hero from "../../components/Hero";
 import FeaturedProject from '../../components/FeaturedProject';
 import Skills from "../../components/Skills";
 import OtherProjects from "../../components/OtherProjects";
+import GithubCalendar from "../../components/GithubCalendar";
 
 function Home() {
     return (
@@ -16,6 +17,7 @@ function Home() {
             <FeaturedProject />          
             <Skills />
             <OtherProjects />
+            <GithubCalendar />
         </Container>
     )
 }
