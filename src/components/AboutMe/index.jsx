db5fe80c38daa4afa57ecb435c46e5902cda8960
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-20 16:11:58 +0300

    Add animations

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import { Container, Row, Col } from "react-bootstrap";
 
@@ -7,17 +10,71 @@ import MyPicture from "../../images/my-picture.jpg";
 import "./index.scss";
 
 function AboutMe() {
+
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+        
+        if (window.innerWidth >= 992) {
+            gsap.from(".my-picture",
+                {
+                    duration: 2.3,
+                    opacity: 0,
+                    x: -110,
+                    scrollTrigger: {
+                        trigger: ".about-me-content-row",
+                        toggleActions: "restart none none pause",
+                        start: "35% bottom"
+                    }
+                }
+            );
+        };
+        gsap.fromTo(".me-in-a-nutshell, .content-paragraph",
+            {
+                opacity: 0,
+                y: 50,
+                scrollTrigger: {
+                    trigger: ".about-me-content-row",
+                    toggleActions: "restart none none pause",
+                    start: "35% bottom"
+                }
+            },
+            {
+                duration: 1.5,
+                opacity: 1,
+                y: 0,
+                scrollTrigger: {
+                    trigger: ".about-me-content-row",
+                    toggleActions: "restart none none pause",
+                    start: "35% bottom"
+                }
+            }
+        );
+        gsap.from(".who-am-i",
+            {
+                delay: 1.5,
+                duration: 3.5,
+                opacity: 0,
+                x: 110,
+                scrollTrigger: {
+                    trigger: ".about-me-content-row",
+                    toggleActions: "restart none none pause",
+                    start: "35% bottom"
+                }
+            }
+        );
+    }, []);
+
     return (
-        <section className="hire-me-section container-fluid px-0 mt-5">
-            <Container className="hire-me-content-container px-0">
-                <Row className="hire-me-content-row">
+        <section className="about-me-section container-fluid px-3 px-sm-0 mt-5">
+            <Container className="about-me-content-container px-0">
+                <Row className="about-me-content-row">
                     <Col xs={12} sm={12} md={12} lg={6}>
-                        <img src={MyPicture} className="img-fluid" alt="me" />
+                        <img src={MyPicture} className="img-fluid my-picture" alt="me" />
                     </Col>
                     <Col xs={12} sm={12} md={12} lg={6}>
-                        <span>Who Am I ?!</span>
-                        <h1>Me In a Nutshell...</h1>
-                        <p>
+                        <span className="who-am-i">Who Am I ?!</span>
+                        <h1 className="me-in-a-nutshell">Me In a Nutshell...</h1>
+                        <p className="content-paragraph">
                             Hello there! I'm Ali Rıza Şahin.
                             <br/>
                             A 19 year old <em>Front End React &amp; Redux Web Developer</em> that tries to make the web better and more accessible place with the help of most recent technologies.
