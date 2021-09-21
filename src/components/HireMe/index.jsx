1006d16410a6e7011b6b495a3ba23837905ee26d
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-21 13:00:16 +0300

    Add starter code for hire me section layout

---
+++ src/components/HireMe/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { Container, Row, Col } from "react-bootstrap";
+
+function HireMe() {
+    return (
+        <section className="hire-me-section container-fluid">
+            <Container className="hire-me-content-container">
+                <Row className="hire-me-content-row">
+                    <Col>
+                    
+                    </Col>
+                    <Col>
+                    
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
+
+export default HireMe
