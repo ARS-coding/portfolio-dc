4e29defdc3f918fbd18d978dba58cd4b8850ea2e
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-21 14:10:04 +0300

    Add initial values and validation prop of formik tag

--- src/components/HireMe/index.jsx
+++ src/components/HireMe/index.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import { Container, Row, Col } from "react-bootstrap";
 
+import { Formik } from "formik";
+
 import "./index.scss";
 
 function HireMe() {
@@ -13,7 +15,36 @@ function HireMe() {
                         
                     </Col>
                     <Col>
-                    
+                        <Formik
+                            initialValues={{
+                                firstName: "",
+                                lastName: "",
+                                email: "",
+                                message: ""
+                            }}
+                            validate={values => {
+                                const errors = {};
+                                if (!values.firstName) {
+                                  errors.firstName = 'Required';
+                                }
+                                if (!values.lastName) {
+                                    errors.lastName = 'Required';
+                                  }
+                                if (!values.email) {
+                                  errors.email = 'Required';
+                                } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+                                  errors.email = 'Invalid email address';
+                                }
+                                if (!values.message) {
+                                    errors.message = 'Required';
+                                } else if (values.message.length > 8000) {
+                                    errors.message = 'Cannot put more that 8000 characters';
+                                }
+                                return errors;
+                            }}
+                        >
+                            
+                        </Formik>     
                     </Col>
                 </Row>
             </Container>
