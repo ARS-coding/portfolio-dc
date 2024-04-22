e80121b4bca933e9049de834ea31285efe9bdad4
Ali Rıza Şahin <arsahin.dev@gmail.com>
2024-04-22 18:43:13 +0300

    use the latest react project initialization technique by utilizing 'createRoot' function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
