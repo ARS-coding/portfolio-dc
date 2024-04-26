6f4a279c7481e4aa8beea5b8d7bc670bef03c401
Ali Rıza Şahin <arsahin.dev@gmail.com>
2024-04-27 01:25:23 +0300

    add robots.txt and sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://ars-coding.netlify.app",
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+  ];
+}
