59fc35cd7828e6b00564ef63bb29892806e65b38
Ali Rıza Şahin <arsahin.dev@gmail.com>
2024-05-09 18:16:03 +0300

    update age

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -67,42 +67,26 @@ function AboutMe() {
             <h1 className="me-in-a-nutshell">Me In a Nutshell...</h1>
             <p className="content-paragraph">
               Hello there! I'm Ali Rıza Şahin.
-              <br />A 19 year old{" "}
-              <em>Front End React &amp; Redux Web Developer</em> that tries to
-              make the web better and more accessible place with the help of
-              most recent technologies.
+              <br />A 21 year old <em>Front End React &amp; Redux Web Developer</em> that tries to make the web better and more accessible place with the help
+              of most recent technologies.
               <br />
-              <br />I graduated from high school in June 2020 and coundn't get
-              into the university that I wanted. I had the idea of preparing for
-              it again but then I stumbled upon an ad of Re:Coded. I can
-              honestly say that, one ad literally changed my life. I applied for
-              the Front End Web Development bootcamp of{" "}
-              <a
-                href="https://www.re-coded.com/"
-                target="_blank"
-                rel="noreferrer"
-              >
+              <br />I graduated from high school in June 2020 and coundn't get into the university that I wanted. I had the idea of preparing for it again but
+              then I stumbled upon an ad of Re:Coded. I can honestly say that, one ad literally changed my life. I applied for the Front End Web Development
+              bootcamp of{" "}
+              <a href="https://www.re-coded.com/" target="_blank" rel="noreferrer">
                 Re:Coded
               </a>{" "}
-              and got selected although the <strong>7% selection rate</strong>{" "}
-              and stopped preparing for the university. I completed this 5
-              months of bootcamp with <strong>43 classes</strong>,{" "}
-              <strong>237 assignments</strong> that I have submitted and{" "}
-              <strong>1100 contributions</strong> that I have done on{" "}
-              <a
-                href="https://github.com/ARS-coding"
-                target="_blank"
-                rel="noreferrer"
-              >
+              and got selected although the <strong>7% selection rate</strong> and stopped preparing for the university. I completed this 5 months of bootcamp
+              with <strong>43 classes</strong>, <strong>237 assignments</strong> that I have submitted and <strong>1100 contributions</strong> that I have done
+              on{" "}
+              <a href="https://github.com/ARS-coding" target="_blank" rel="noreferrer">
                 GitHub
               </a>
               .
               <br />
-              <br />I can't get enough of seeing the results of my code on the
-              browser and it's literally the <em>best feeling</em> in the world!
+              <br />I can't get enough of seeing the results of my code on the browser and it's literally the <em>best feeling</em> in the world!
               <br />
-              I'm just really <em>happy and grateful</em> that I found the thing
-              that I would look forward to do after I wake up <em>everyday</em>.
+              I'm just really <em>happy and grateful</em> that I found the thing that I would look forward to do after I wake up <em>everyday</em>.
             </p>
           </Col>
         </Row>
